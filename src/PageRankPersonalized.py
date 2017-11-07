5cc9fa63bfcff1745a8ede1647dcfed1db48d292
Mingfei Yang <Mingfei.Yang.1@asu.edu>
2017-11-07 15:44:00 -0700

    Iteration Stop Delta Completed Increasing the Stop Iteration Steps to 50 and Setting up comparisons with Stop Delta

--- src/PageRankPersonalized.py
+++ src/PageRankPersonalized.py
@@ -4,11 +4,11 @@
 import numpy as np
 
 # Iteration Time for Random Walk
-# Default - Iteration Time is Set to 20
-ITERATION_TIME = 20
+# Default - Iteration Time is Set to 50
+ITERATION_TIME = 50
 # Iteration Delta for Random Walk
-# Default - Iteration Delta is Set to (1e-10) / (1 * 10 ** -10)
-ITERATION_DELTA = 1e-10
+# Default - Iteration Delta is Set to (1.0e-10) / (1.0 * 10 ** -10)
+ITERATION_DELTA = 1.0e-10
 
 
 # Personalized PageRank Function
@@ -41,10 +41,22 @@ def personalized_page_rank(seeds, trans_2d_list, alpha=0.9):
     # pr_new = alpha * TransitionMatrix * pr_old + (1 - alpha) * SeedVector
     for i in range(0, ITERATION_TIME):
         # Set Previous PR Vector
-        prev_pr_vector = pr_vector
+        prev_vector = pr_vector
         # Calculate New PR Vector
-        pr_vector = alpha * trans_matrix * prev_pr_vector + (1 - alpha) * seed_vector
-        # Compare the Difference Between Previous and New Vectors
+        pr_vector = alpha * trans_matrix * prev_vector + (1 - alpha) * seed_vector
+
+        # Optional - Compare the Difference Between Previous and New Vectors
+        # Get the absolute value of All raw difference
+        pr_diff = (np.absolute(prev_vector - pr_vector)).tolist()
+        # If the difference in each dimension is less than ITERATION_DELTA, break
+        count = 0  # Counter for values that satisfy (diff < DELTA)
+        print(pr_diff)
+        for j in range(0, row_num):
+            if pr_diff[j][0] < ITERATION_DELTA:
+                count += 1
+        if count == row_num:
+            break
+        # Compare ITERATION DELTA End
 
     pr_list = pr_vector.tolist()
     # Return the Final PageRank Score Vector
